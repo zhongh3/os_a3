022247383564bedc7cda9d8c6e14138e0a1c1ac6
zhongh3 <zhon0017@gmail.com>
2019-03-26 02:12:08 +0800

    implement <who> in Task 1

--- a3.c
+++ a3.c
@@ -3,10 +3,13 @@
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
+static char *who = "world";
+module_param(who, charp, S_IRUGO);
+
 
 static int hello_init(void)
 {
-    printk(KERN_ALERT "Hello, world!\n");
+    printk(KERN_ALERT "Hello, %s!\n", who);
     return 0;
 }
 
